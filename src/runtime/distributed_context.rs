bc913b966b4f53e430a5b05848b759c36dbb37b3
David Porkka <37228537+dporkka@users.noreply.github.com>
2026-09-20 21:42:41 -0300

    feat(fabric): make untracked remote publish non-blocking

--- src/runtime/distributed_context.rs
+++ src/runtime/distributed_context.rs
@@ -969,12 +969,16 @@ impl Runtime {
                             MessageAdmission::Rejected => report.rejected += 1,
                         }
                     } else {
-                        self.send_distributed(
+                        match self.try_send_distributed(
                             ActorAddress::remote(node_id, actor_id),
                             &behavior,
                             args,
-                        );
-                        report.forwarded_remote += 1;
+                        ) {
+                            MessageAdmission::Accepted => report.admitted += 1,
+                            MessageAdmission::Forwarded => report.forwarded_remote += 1,
+                            MessageAdmission::Backpressured => report.backpressured += 1,
+                            MessageAdmission::Rejected => report.rejected += 1,
+                        }
                     }
                 }
             }
