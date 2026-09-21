a7793ed5597336b4d5f70ef3f29024795b903329
David Porkka <37228537+dporkka@users.noreply.github.com>
2026-09-21 18:14:23 -0300

    data: expose logical query planner

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@ pub mod core_vm;
 #[cfg(feature = "native-codegen")]
 pub mod cranelift_utils;
 pub mod dap;
+pub mod data_plan;
 pub mod diagnostic;
 #[cfg(feature = "native-codegen")]
 pub mod difffuzz;
